4dd3b30913002f9fabf3a129faef7a96ca4b966c
Ecthon Borhis <ecthon@gmail.com>
2021-05-24 10:15:59 -0400

    Add Google Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+ import { GlobalStyle } from "./styles/global";
+ 
  export function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
+      <h1>Hello, Sir!</h1>
     </div>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,7 +14,7 @@ export const GlobalStyle = createGlobalStyle`
         --background: #F0F2F5;
         --shape: #FFFFFF;
     }
-    
+
    * {
        margin: 0;
        padding: 0;
@@ -35,6 +35,15 @@ export const GlobalStyle = createGlobalStyle`
    body {
        background: var(--background);
        -webkit-font-smooth: antialiased;
+   }
+
+   body, input, textarea, button {
+       font-family: 'Poppins', sans-serif;
+       font-weight: 400;
+   }
+
+   h1, h2, h3, h4, h5, h6, strong {
+       font-weight: 600;
    }
 
    button {
