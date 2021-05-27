47fdef97e22ff545447fb058a90eac1bdb57f292
Ecthon Borhis <ecthon@gmail.com>
2021-05-27 09:50:18 -0400

    Colors on the button using the "polished" package

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -56,14 +56,24 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
     isActive: boolean;
+    activeColor: 'green' | 'red';
 }
+
+const colors = {
+    green: '#33cc95',
+    red: '#E52E4D',
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: ${(props) => props.isActive ? '#000' : 'transaparent'};
+        background: ${(props) => props.isActive
+        ? transparentize(0.9, colors[props.activeColor])
+        : 'transaparent'
+        };
+
         display: flex;
         align-items: center;
         justify-content: center;
